848fd99a23223804cee0ec0b632a2d03c2f99137
Stradioto <45260065+Stradioto@users.noreply.github.com>
2026-03-18 11:00:42 +0100

    Add files via upload

---
+++ Basics/ConditionalInclusion/moreonfileseparation/test.cc
@@ -0,0 +1,7 @@
+#include "test.h"
+#include <iostream>
+using namespace std;
+void test(int x) // definition
+{
+cout << x << endl;
+}
---
+++ Basics/ConditionalInclusion/moreonfileseparation/test.h
@@ -0,0 +1,4 @@
+#ifndef TEST_H
+#define TEST_H
+void test(int x = 0); // declaration
+#endif//TEST_H
